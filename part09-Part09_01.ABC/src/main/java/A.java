e44f2bccb0314f46142b243abfffb9f72eacb7b0
Mike <giorgiomotorola12@gmail.com>
2022-03-22 12:39:50 -0400

    finished part 8

---
+++ part09-Part09_01.ABC/src/main/java/A.java
@@ -0,0 +1,8 @@
+
+public class A {
+    
+    
+    public void a() {
+        System.out.println("A");
+    }
+}
--- part09-Part09_01.ABC/src/main/java/Main.java
+++ part09-Part09_01.ABC/src/main/java/Main.java
@@ -2,7 +2,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write your test code here
-        
+        C c = new C();
+
+        c.a();
+        c.b();
+        c.c();
+
     }
 }
