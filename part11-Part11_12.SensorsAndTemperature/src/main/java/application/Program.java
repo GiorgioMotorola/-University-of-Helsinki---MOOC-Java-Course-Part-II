a09fccaa559ac87573a2b6630cb8511d258f7dee
Mike <giorgiomotorola12@gmail.com>
2022-03-28 17:30:42 -0400

    finished part 11

--- part11-Part11_12.SensorsAndTemperature/src/main/java/application/Program.java
+++ part11-Part11_12.SensorsAndTemperature/src/main/java/application/Program.java
@@ -3,8 +3,16 @@ package application;
 public class Program {
 
     public static void main(String[] args) {
-        // you can test your classes here:
-        
+        StandardSensor ten = new StandardSensor(10);
+        StandardSensor minusFive = new StandardSensor(-5);
+
+        System.out.println(ten.read());
+        System.out.println(minusFive.read());
+
+        System.out.println(ten.isOn());
+        ten.setOff();
+        System.out.println(ten.isOn());
+
     }
 
 }
